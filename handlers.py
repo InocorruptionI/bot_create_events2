3f129a10a0f1beea21bf5c884c47682307846f37
Konstantin <kuzmin.konstantin70@gmail.com>
2025-06-19 16:31:44 +0300

    create_new_handlers

--- handlers.py
+++ handlers.py
@@ -1,7 +1,7 @@
 import html
 import re
 
-
+from aiogram.enums import ParseMode
 from redis import Redis
 import json
 
@@ -24,26 +24,34 @@ router = Router()
 
 
 class Form(StatesGroup):
-    type_event = State()
-    event_date = State()
-    num_guests = State()
+    type_event = State()#Тип мероприятия
+    event_date = State() #Дата проведения мероприятия
+    venue = State()  #Место проведения
+    num_guests = State()  #Количество гостей
+    budget_event = State()  # Место проведения
+    atmosphere = State()  #Какая будет атмосфера
+    specific_topic = State()  # Есть ли конкретная тема мероприятия
     event_info = State()
     contact_phone = State()
 
 
 class DataEvent:
     def __init__(self):
-        self.type = None
-        self.date = None
-        self.num_guests = None
+        self.type = None #Тип мероприятия
+        self.date = None #Дата проведения мероприятия
+        self.venue = None #Место проведения
+        self.budget_event = None #Бюджет мероприятия
+        self.num_guests = None #Количество гостей
+        self.atmosphere = None #Какая будет атмосфера
+        self.specific_topic = None #Есть ли конкретная тема мероприятия
         self.info = None
         self.phone = None
 
 
 @router.message(CommandStart())
 async def start(message: Message):
-    '''Данная функция представляет собой приветствие для разных типов пользователей:
-    для администраторов и обычных пользователей'''
+    #Данная функция представляет собой приветствие для разных типов пользователей:
+    #для администраторов и обычных пользователей
     admin_id = 498037596
     if message.from_user.id == admin_id:
         await message.answer(text='Привет Администратор! \n\n Выбери действие.', reply_markup=keyboards.main_admin_kb)
@@ -57,23 +65,25 @@ async def start(message: Message):
 async def create_event(message: Message, state: FSMContext) -> None:
     await state.update_data()
     await state.set_state(Form.type_event)
-    await message.answer(text='Введите тип мероприятия: Wedding, Birthday, Corporate, '
-                              'Свадьба, День рождения, Корпоратив',
-                         reply_markup=ReplyKeyboardRemove())
+    await message.answer(text='<b>Часть 1. Общая информация о мероприятии:</b> \n'
+                              'Какой тип мероприятия планируется? (Свадьба, Корпоратив, Юбилей, День рождения)\n'
+                              '*Зачем*: Определяет стиль ведения, тон мероприятия\n'
+                              '(формальный, неформальный, торжественный) и общую атмосферу. ',
+                         reply_markup=ReplyKeyboardRemove(), parse_mode=ParseMode.HTML)
 
 
 @router.message(Form.type_event)
 async def add_event_type(message: Message, state: FSMContext) -> None:
-    walid_types = ['wedding', 'birthday', 'corporate', 'свадьба', 'день рождения', 'корпоратив']
+    walid_types = ['cвадьба', 'день рождения', 'корпоратив', 'юбилей']
     if message.text.lower() not in walid_types:
         await message.answer(text='Ваш вариант не подходит ни под один из предложенных. Пожалуйста выберите '
-                                  'тип из предложенных: Wedding, Birthday, Corporate, '
+                                  'тип из предложенных: '
                                   'Свадьба, День рождения, Корпоратив')
         return
     await state.update_data(type_event=message.text)
     await state.set_state(Form.event_date)
     await message.answer(text=f'Отлично! Мы начали создание мероприятия "{html.escape(message.text)}"! Едем дальше,'
-                              f'напиши дату мероприятия в формате ГГГГ-ММ-ДД')
+                              f'Дата и время проведения? В формате ГГГГ-ММ-ДД')
 
 
 @router.message(Form.event_date)
@@ -81,34 +91,51 @@ async def add_event_date(message: Message, state: FSMContext):
     try:
         year, month, day = list(map(int, message.text.split('-')))
         valid_str = message.text.replace('-', '').isdigit()
-        if not valid_str and not re.search(r'^\d{4}-\d{2}-\d{2}$', message.text) or \
-                datetime.now() >= datetime(year, month, day):
-            await message.answer(text='Что-то тут вы ввели не так, давайте попробуем еще раз! Пример: ГГГГ-ММ-ДД')
+        valid_len = message.text.replace('-', '')
+        if not valid_str and not re.search(r'^\d{4}-\d{2}-\d{2}$', message.text) and \
+                datetime.now() >= datetime(year, month, day) or valid_len != 8:
+            await message.answer(text='Что-то тут вы ввели не так, пробуем еще раз! Пример: ГГГГ-ММ-ДД')
             return
+        else:
+            await state.update_data(event_date=message.text)
+            await state.set_state(Form.venue)
+            await message.answer(text='Я записал!\n'
+                                      '<b>Укажите место проведения:</b> (ресторан, открытая площадка, '
+                                      'загородный дом и т.д. в свободной форме)\n'
+                                      '<s>*Зачем*:</s> Влияет на технические аспекты (звук, свет, сцена), логистику и '
+                                      'взаимодействие с персоналом площадки.', parse_mode=ParseMode.HTML)
     except ValueError:
         await message.answer(text='Что-то тут вы ввели не так, давайте попробуем еще раз! Пример: ГГГГ-ММ-ДД')
         return
 
-    await state.update_data(event_date=message.text)
+
+@router.message(Form.venue)
+async def add_venue(message: Message, state: FSMContext):
+    await state.update_data(venue=message.text)
     await state.set_state(Form.num_guests)
-    await message.answer(text='Я записал!, Введие количесво гостей в формате (пример: 32)')
+    await message.answer(text='Укажите количество гостей\n'
+                              '*Зачем*: Определяет масштаб мероприятия, выбор активностей, '
+                              'количество необходимых ресурсов (например, микрофонов, мест для рассадки).')
 
 @router.message(Form.num_guests)
 async def add_num_guests(message: Message, state: FSMContext):
     if message.text.isdigit():
         await state.update_data(num_guests=int(message.text))
-        await state.set_state(Form.event_info)
-        await message.answer(text='Отлично, двигаемся дальше! Теперь вам нужно рассказать все что должно входить в '
-                                  'состав мероприятия, а именно: Формат: что вы именно хотите увидеть на мероприятии\n'
-                                  'Имена гостей: желательно ввести гостей по значимости, от самого значимого (мама/папа) '
-                                  'к менее значимым\n'
-                                  'Мелочи: те самые мелочи которые вы хотели бы видеть\n'
-                                  'Не переживайте, после заполнения заявки я с вами свяжусь и мы уже вместе пройдемся '
-                                  'по заявке :)')
+        await state.set_state(Form.budget_event)
+        await message.answer(text='Укажите бюджет мероприятия\n '
+                                  'Зачем*: Помогает понять, какие услуги и шоу-программы можно предложить, '
+                                  'чтобы уложиться в финансовые рамки.')
     else:
         await message.answer(text='Вы ввели неправильный формат. Пожалуйста введите количество гостей согласно примеру.'
                                   '(Пример: 31)')
         return
+
+
+@router.message(Form.budget_event)
+async def add_budget(message: Message, state: FSMContext):
+    await state.update_data(budget_event=message.text)
+    await state.set_state(Form.atmosphere)
+    await message.answer(text='Какую атмосферу вы хотите создать?')
 
 
 @router.message(Form.event_info)
@@ -152,17 +179,17 @@ async def add_contact_phone(message: Message, state: FSMContext):
         redis_client.hset(user_key, user_event_key, json.dumps(event_data, ensure_ascii=False))
 
 
-        event_text = (f"Мероприятие создано!\n"
-                      f"Тип: {data['type_event']}\n"
-                      f"Дата: {data['event_date']}\n"
-                      f"Гостей: {data['num_guests']}\n"
-                      f"Информация: {data['event_info']}\n"
-                      f"Телефон: {data['contact_phone']}")
+        event_text = (f"<b>Мероприятие создано!</b>\n"
+                      f"<b>Тип:</b> {data['type_event']}\n"
+                      f"<b>Дата:</b> {data['event_date']}\n"
+                      f"<b>Гостей:</b> {data['num_guests']}\n"
+                      f"<b>Информация:</b> {data['event_info']}\n"
+                      f"<b>Телефон:</b> {data['contact_phone']}")
         id_admin = 498037596
         if id_admin == message.from_user.id:
-            await message.answer(text=event_text, reply_markup=keyboards.main_admin_kb)
+            await message.answer(text=event_text, reply_markup=keyboards.main_admin_kb, parse_mode=ParseMode.HTML)
         else:
-            await message.answer(text=event_text, reply_markup=keyboards.main_user_kb)
+            await message.answer(text=event_text, reply_markup=keyboards.main_user_kb, parse_mode=ParseMode.HTML)
 
         await state.clear()
     else:
@@ -202,7 +229,8 @@ async def check_keys_events(message: Message):
         if not keys:
             await message.answer(text='У вас еще нет мероприятий')
             return
-        await message.answer(text='Выберите мероприятие:', reply_markup=keyboards.get_events_keys(info, real_key=keys))
+        else:
+            await message.answer(text='Выберите мероприятие:', reply_markup=keyboards.get_events_keys(info, real_key=keys))
 
 
 
@@ -213,14 +241,14 @@ async def info_key_for_user(callback: CallbackQuery):
     id_user = callback.from_user.id # id юзера
     event_info = json.loads(redis_client.hget(f'user:{id_user}', event_key))
 
-    responses = (f'Тип мероприятия: {event_info['type']}\n'
-                 f'Дата проведения: {event_info['date']}\n'
-                 f'Количество гостей: {event_info['num_guests']}\n'
-                 f'Информация о мероприятии: {event_info['event_info']}\n'
-                 f'Номер телефона: {event_info['telephone']}\n\n'
+    responses = (f'<b>Тип мероприятия:</b> {event_info['type']}\n'
+                 f'<b>Дата проведения:</b> {event_info['date']}\n'
+                 f'<b>Количество гостей:</b> {event_info['num_guests']}\n'
+                 f'<b>Информация о мероприятии:</b> {event_info['event_info']}\n'
+                 f'<b>Номер телефона:</b> {event_info['telephone']}\n\n'
     )
 
 
-    await callback.message.answer(text=f'Ваше мероприятие: \n\n{responses}')
+    await callback.message.answer(text=f'<b>Ваше мероприятие:</b> \n{responses}', parse_mode=ParseMode.HTML)
     await callback.answer()
 
